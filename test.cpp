c0afddefd162f81d7eca781ccf41da636c5f99a8
chenzhaohui <czh8706@163.com>
2020-08-15 20:30:22 +0800

    add get address of parameter

--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
 
+#include <iostream>
+#include <stdio.h>
 
 int main()
 {
    std::cout<<"Hello word!"<<std::endl;
+   char a = '2';
+   char *p = &a;
+   std::cout<<*p<<std::endl;
+   printf("%p\n", p);
+   std::cout<<p<<std::endl;   
    return 0;
 }
